784a7f0c910e037544ed510b8eb742bc9afdcd62
Jahguy <obonyo.official@gmail.com>
2017-04-19 16:25:22 +0300

    Create HelloWorld.java
    A java program to print 'Hello World' to screen

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;//package name
+
+/**
+ *
+ * @author JAHGUY
+ */
+public class HelloWorld {//
+    public static void main(String[]args){//main method
+        System.out.println("Hello World!");//print 'Hello World' to screen
+    }//end main method
+    
+}//end class
